7eba4cb141e9e31d13c1fccb4743265a8355f0f5
Mohmaed El Serafy <elserafy02@gmail.com>
2018-08-04 19:16:33 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 // Code goes here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = React.createElement('h1',{},'My first React Code');
+ReactDOM.render(
+  title,
+  document.getElementById('global')
+);
+const paragraph = React.createElement('p', {} , 'Writing some more HTML. Cool stuff' );
+const container = React.createElement('div', {} , [title , paragraph]);
+ReactDOM.render(container , document.getElementById('global')
+
+);
+
+const list = React.createElement('div',{},React.createElement('h1',{},'My favorite ice cream flavors')
+, React.createElement('ul',{} ,
+
+[
+  React.createElement('li',{class :'while'},'Chocolate'),
+
+  React.createElement('li',{class:'white'},'Banana'),
+
+React.createElement('li',{class:'yellow'},'Vanilla')
+]
+)
+);
+ReactDOM.render(
+  list,
+  document.getElementById('global')
+);
