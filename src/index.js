1eb9c5632a1ee6387056c47684428eff88d44962
Martin Beletskyi-Moawad <martbelmoaw@gmail.com>
2018-10-12 08:43:40 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-// Code goes here
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const title = React.createElement('h1', {}, 'Test!');
+const paragraph = React.createElement('p', {}, 'Testing paragraphs');
+const container = React.createElement('div', {}, [title, paragraph]);
+
+ReactDOM.render(container, document.getElementById('global'));
