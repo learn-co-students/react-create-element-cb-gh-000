3fffdb46c26a6082263f2eb8d2d68074c7ce045e
Amiruddin Marmul <amirmarmul@gmail.com>
2018-11-11 11:50:08 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 // Code goes here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const container = React.createElement('div', {},
+  React.createElement('h1', {}, 'Makanan Khas Makassar'),
+  React.createElement('ul', {}, [
+    React.createElement('li', {}, 'Coto'),
+    React.createElement('li', {}, 'Jalangkote'),
+    React.createElement('li', {}, 'Konro')
+  ])
+);
+
+ReactDOM.render(
+  container,
+  document.getElementById('global')
+);
